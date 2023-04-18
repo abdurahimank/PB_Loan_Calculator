8c5b9fa0d3468573e49f46840ef5ce56a564c301
ABDURAHIMAN K <laluvga@gmail.com>
2023-04-18 09:51:47 +0530

    Stage 4/4: Differentiate payment

--- stages.py
+++ stages.py
@@ -1,13 +1,11 @@
-# Stage 3/4: Annuity payment
+# Stage 4/4: Differentiate payment
 import math
+import argparse
 
 
-def find_no_months():
-    loan_principal = float(input("Enter the loan principal:\n"))
-    monthly_payment = float(input("Enter the monthly payment:\n"))
-    interest = float(input("Enter the loan interest:\n"))
-    i = interest / (12 * 100)
-    no_months = math.ceil(math.log(monthly_payment / (monthly_payment - (i * loan_principal)), 1 + i))
+def find_no_months(p, m, i):  # p = principal, m = monthly_payment, i = interest
+    i = i / (12 * 100)
+    no_months = math.ceil(math.log(m / (m - (i * p)), 1 + i))
     if no_months == 1:
         print("It will take 1 month to repay the loan")
     elif no_months < 12:
@@ -20,33 +18,52 @@ def find_no_months():
         print(f"It will take {no_months // 12} years and 1 month to repay this loan!")
     else:
         print(f"It will take {no_months // 12} years and {no_months % 12} months to repay this loan!")
+    print(f"Overpayment = {round((m * no_months) - p)}")
 
 
-def find_monthly_payment():
-    loan_principal = float(input("Enter the loan principal:\n"))
-    no_months = float(input("Enter the number of periods:\n"))
-    interest = float(input("Enter the loan interest:\n"))
-    i = interest / (12 * 100)
-    monthly_payment = math.ceil(loan_principal * ((i * ((1 + i) ** no_months)) / (((1 + i) ** no_months) - 1)))
+def find_monthly_payment(p, n, i):  # p = principal, n = periods, i = interest
+    i = i / (12 * 100)
+    monthly_payment = math.ceil(p * ((i * ((1 + i) ** n)) / (((1 + i) ** n) - 1)))
     print(f"Your monthly payment = {monthly_payment}!")
+    print(f"Overpayment = {round(p - (n * monthly_payment))}")
 
 
-def find_loan_principal():
-    monthly_payment = float(input("Enter the annuity payment:\n"))
-    no_months = float(input("Enter the number of periods:\n"))
-    interest = float(input("Enter the loan interest:\n"))
-    i = interest / (12 * 100)
-    loan_principal = math.floor(monthly_payment / ((i * ((1 + i) ** no_months)) / (((1 + i) ** no_months) - 1)))
+def find_loan_principal(m, n, i):  # m = monthly_payment, n = periods, i = interest
+    i = i / (12 * 100)
+    loan_principal = math.floor(m / ((i * ((1 + i) ** n)) / (((1 + i) ** n) - 1)))
     print(f"Your loan principal = {loan_principal}!")
+    print(f"Overpayment = {round((m * n) - loan_principal)}")
 
 
-to_calculate = input("""What do you want to calculate?
-type "n" for number of monthly payments,
-type "a" for annuity monthly payment amount,
-type "p" for loan principal:\n""")
-if to_calculate == "n":
-    find_no_months()
-elif to_calculate == "a":
-    find_monthly_payment()
-else:
-    find_loan_principal()
+def diff_payment(p, n, i):  # p = principal, n = periods, i = interest
+    i = i / (12 * 100)
+    total_amount = 0
+    for m in range(1, n + 1):
+        nth_payment = math.ceil((p / n) + (i * (p - ((p * (m - 1)) / (n)))))
+        print(f"Month {m}: payment is {nth_payment}")
+        total_amount += nth_payment
+    print(f"\nOverpayment = {round(total_amount - p)}")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--type")
+parser.add_argument("--payment")
+parser.add_argument("--principal")
+parser.add_argument("--periods")
+parser.add_argument("--interest")
+args = parser.parse_args()
+parameters = dict(type=args.type, payment=0 if args.payment is None else float(args.payment),
+                  principal=0 if args.principal is None else float(args.principal),
+                  periods=0 if args.periods is None else int(args.periods),
+                  interest=0 if args.interest is None else float(args.interest))
+if args.type not in ["annuity", "diff"] or (args.type == "diff" and args.payment) or args.interest is None \
+        or list(parameters.values()).count(0) > 1 or any([True for i in list(parameters.values())[1:] if i < 0]):
+    print("Incorrect parameters")
+elif parameters["type"] == "diff":
+    diff_payment(parameters["principal"], parameters["periods"], parameters["interest"])
+elif parameters["type"] == "annuity" and parameters["principal"] == 0:
+    find_loan_principal(parameters["payment"], parameters["periods"], parameters["interest"])
+elif parameters["type"] == "annuity" and parameters["payment"] == 0:
+    find_monthly_payment(parameters["principal"], parameters["periods"], parameters["interest"])
+elif parameters["type"] == "annuity" and parameters["periods"] == 0:
+    find_no_months(parameters["principal"], parameters["payment"], parameters["interest"])
